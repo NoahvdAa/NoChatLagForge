85de6a6e5f15f120bf07368b6215045262f699dd
Noah van der Aa <ndvdaa@gmail.com>
2022-01-23 11:11:00 +0100

    1.16 support

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# NoChatLagForge
+# NoChatLagForge [1.16 branch]
 
 [Fabric Version](https://github.com/NoahvdAa/NoChatLag) | [Plugin Version](https://github.com/NoahvdAa/NoChatLagServer)
 
@@ -7,3 +7,5 @@ Downloads: [Modrinth](https://modrinth.com/mod/nochatlag) | [CurseForge](https:/
 A couple of days ago, something changed in Mojang's blocked users API, causing the client to not properly load the list of blocked users on start. Because of this, the client will attempt to fetch the blocklist every two minutes when the chat is being rendered. This loading is done on the same thread as the rendering, causing the game to freeze until Mojang's API returns a response. Since the requests takes a while to complete, this will cause a noticeable lag spike.
 
 This issue is being tracked in Mojira issue [WEB-5587](https://bugs.mojang.com/browse/WEB-5587).
+
+You are currently viewing the 1.16.5 branch. Check out the *master* branch for the 1.17/1.18 version.
--- src/main/java/me/noahvdaa/nochatlag/forge/mixin/GuiMixin.java
+++ src/main/java/me/noahvdaa/nochatlag/forge/mixin/GuiMixin.java
@@ -1,9 +1,9 @@
 package me.noahvdaa.nochatlag.forge.mixin;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.chat.ChatListener;
-import net.minecraft.network.chat.ChatType;
-import net.minecraft.network.chat.Component;
+import net.minecraft.client.gui.chat.IChatListener;
+import net.minecraft.util.text.ChatType;
+import net.minecraft.util.text.ITextComponent;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -17,7 +17,7 @@ import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Mixin(net.minecraft.client.gui.Gui.class)
+@Mixin(net.minecraft.client.gui.IngameGui.class)
 public abstract class GuiMixin {
 
 	private final ExecutorService service = Executors.newFixedThreadPool(1);
@@ -28,22 +28,22 @@ public abstract class GuiMixin {
 
 	@Final
 	@Shadow
-	protected Map<ChatType, List<ChatListener>> chatListeners;
+	protected Map<ChatType, List<IChatListener>> chatListeners;
 
 	@Shadow
-	public abstract UUID guessChatUUID(Component message);
+	public abstract UUID guessChatUUID(ITextComponent message);
 
 	@Inject(
-			method = "handleChat(Lnet/minecraft/network/chat/ChatType;Lnet/minecraft/network/chat/Component;Ljava/util/UUID;)V",
+			method = "handleChat(Lnet/minecraft/util/text/ChatType;Lnet/minecraft/util/text/ITextComponent;Ljava/util/UUID;)V",
 			at = @At("HEAD"),
 			cancellable = true
 	)
-	public void handleChat(ChatType chatType, Component chatComponent, UUID senderUUID, CallbackInfo ci) {
+	public void handleChat(ChatType chatType, ITextComponent chatComponent, UUID senderUUID, CallbackInfo ci) {
 		service.submit(() -> {
 			if (this.minecraft.isBlocked(senderUUID) || (this.minecraft.options.hideMatchedNames && this.minecraft.isBlocked(this.guessChatUUID(chatComponent)))) {
 				return;
 			}
-			for (ChatListener chatListener : this.chatListeners.get(chatType)) {
+			for (IChatListener chatListener : this.chatListeners.get(chatType)) {
 				chatListener.handle(chatType, chatComponent, senderUUID);
 			}
 		});
